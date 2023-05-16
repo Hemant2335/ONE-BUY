0aff56d7961115ac3de259d954fbfdb3a1731e88
Hemant2335 <knrt73373@gmail.com>
2023-05-16 21:31:09 +0530

    commit

--- app/category/[slug]/page.js
+++ app/category/[slug]/page.js
@@ -1,13 +1,26 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useRouter } from 'next/navigation';
 import { useParams } from 'next/navigation';
 import ProductCard from '@/components/ProductCard';
 import Wrapper from '@/components/Wrapper';
+import { Context } from '@/app/Context/User';
+import { useContext } from 'react';
+
+
 const Category = () => {
   const params = useParams();
-  const {slug} = params
+  const {slug} = params;
+
+  const {category} = useContext(Context);
+
+  useEffect=(()=>{
+
+    
+
+  },[])
+
   return (
     <Wrapper>
     <h1 className='text-4xl text-center mt-10'>{slug}</h1>
